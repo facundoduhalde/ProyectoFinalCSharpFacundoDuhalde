184e74d1be096bfb30791682d740d75cbcc26681
facundoduhalde <facundo.duhalde@gmail.com>
2023-05-11 09:42:20 -0300

    Agregar archivos de proyecto.

---
+++ ProyectoFinalCSharpFacundoDuhalde/Producto.cs
@@ -0,0 +1,18 @@
+﻿namespace ProyectoFinalCSharpFacundoDuhalde
+{
+    public class Producto
+    {
+        //Id, Descripcion, Costo, PrecioVenta, Stock, IdUsuario
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public double Costo { get; set; }
+        public double PrecioVenta { get; set; }
+        public int Stock { get; set; }
+        public int IdUsuario { get; set; }
+
+        public Producto() 
+        { 
+            Descripcion = string.Empty; 
+        }
+    }
+}
---
+++ ProyectoFinalCSharpFacundoDuhalde/Venta.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalCSharpFacundoDuhalde
+{
+    internal class Venta
+    {
+        // Id, Comentarios, IdUsuario
+        public int Id { get; set; }
+        public string Comentarios { get; set; }
+        public int IdUsuario { get; set; }
+
+        public Venta()
+        {
+            Comentarios = string.Empty; 
+
+        }
+    }
+}
